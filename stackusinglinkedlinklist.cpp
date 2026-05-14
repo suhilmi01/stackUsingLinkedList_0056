344377b5f4f132e0db8d8256bf1b9ad1ef761d49
suhilmi01 <suhilmi01@mail.com>
2026-05-14 18:41:34 +0700

    untuk menghapus data paling atas pada stack, dan akan menampilkan pesan jika stack kosong

--- stackusinglinkedlinklist.cpp
+++ stackusinglinkedlinklist.cpp
@@ -33,3 +33,10 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
