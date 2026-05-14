5b8fcef1ac75409f07aff98ff6fd395d8c18ae23
suhilmi01 <suhilmi01@mail.com>
2026-05-14 18:42:04 +0700

    menyimpan sementara node paling atas, memindahkan top ke node berikutnya, lalu menampilkan data yang dihapus dari stack

--- stackusinglinkedlinklist.cpp
+++ stackusinglinkedlinklist.cpp
@@ -40,3 +40,8 @@ public:
         {
             cout << "Stack is empty." << endl;
         }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+    }
