f3c16456cdd9c47e887951a61523e8254021de4b
suhilmi01 <suhilmi01@mail.com>
2026-05-14 18:49:20 +0700

    untuk menghapus data teratas stack jika tidak kosong, jika kosong akan menampilkan pesan error

--- stackusinglinkedlinklist.cpp
+++ stackusinglinkedlinklist.cpp
@@ -94,6 +94,16 @@ int main()
             stack.push(value);
             break;
 
+            case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
+            else
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
         }
     }
 }
